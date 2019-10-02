60cf605beaa8ab7fae967041cc89a5e142b2a4ac
kam440 <kameswaribhavya@gmail.com>
2019-10-01 17:48:26 -0700

    good

---
+++ 1-10-2019/Packages/operations.py
@@ -0,0 +1,26 @@
+def addition(a,b):
+    c=a+b
+    print(c)
+def prime(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count=count+1
+    if count==2:
+        print("true")
+    else:
+        print("false")
+
+        
+        
+        
+def primefact(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count=count+1
+            print(i)
+    if count==2:
+        print("true")
+    else:
+        print("false")
